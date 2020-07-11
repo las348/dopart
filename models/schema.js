5a2ea860733895b5ff85dd7f56bdb2dfa9cf9212
Laureni Wilkinson <las348@nyu.edu>
2020-07-11 13:02:16 -0400

    schema

---
+++ models/schema.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    date: {
+        type: Date,
+        required: "Date is Required"
+    },
+    exercises: [{
+        type: {
+            type: String,
+            required: "Type is Required"
+        },
+        name: {
+            type: String,
+            required: "Name is Required"
+        },
+        duration: {
+            type: Number,
+            required: "Duration is Required"
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
